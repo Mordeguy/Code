62357bc98c1a29c729189a3fecdb70c313a7d348
Mordeguy <nathan.abbey@gmail.com>
2020-10-09 16:59:15 -0400

    JavaScript linked list

---
+++ JS/linked_list/linkedList.js
@@ -0,0 +1,81 @@
+/* *************************************************************
+ *
+ *	   Name:   linkedList.js
+ *	 Author:   Nathan M. Abbey
+ *
+ *	 Purpose:   Displays singular linked list functionality
+ *  Required:   Node.js
+ *
+ * *************************************************************
+ */
+const ListNode = require('./Node');
+
+class LinkedList {
+
+    constructor(){
+        this.head = null; 
+    }
+
+
+    addNodeToHead(dataIn) {
+        let currentHead = this.head;
+        let newHead = new ListNode(dataIn);
+        this.head = newHead;
+        newHead.next = currentHead;
+    }
+
+    removeHead() {
+        let nodeHead = this.head;
+        if(nodeHead === null){
+            return;
+        }
+        this.head = nodeHead.getNext();
+    }
+
+    addNodeToTail(dataIn) {
+        let nodeTail = this.head;
+        let node = new ListNode(dataIn);  
+        
+        if (nodeTail) {
+            while (nodeTail.getNext() !== null) {
+                nodeTail = nodeTail.getNext();
+              }
+              nodeTail.setNext(node);
+        } else {
+            this.head = node;
+      }
+    }       
+
+
+
+
+    printListData() {
+        let headNode = this.head;
+        let out = '';
+
+        while (headNode !== null){
+            out += headNode.data + ' ';
+            headNode = headNode.getNext();
+        }
+        console.log(out);
+    }
+}
+
+
+
+const list = new LinkedList();
+
+
+
+list.addNodeToTail('Cold');
+list.addNodeToTail('Steve');
+list.addNodeToTail('Austin')
+list.addNodeToHead('Stone');
+
+
+
+
+
+list.printListData();
+
+
